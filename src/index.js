b2ffceaf7a5be8e686440356a4047c6a3d6195ae
David Cisneros <d.cisneros849@gmail.com>
2017-12-11 19:37:14 -0500

    react hello world works now

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const css = require('./app.css')
 
-console.log("hello there");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, World!</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
+var path = require("path");
+
 module.exports = {
   entry: './src/index.js',
   output: {
-      filename: './dist/app.bundle.js'
+       path: path.resolve(__dirname, "dist"),
+      filename: 'app.bundle.js'
   },
   module: {
     rules: [
-       {test: /\.css$/, loaders: 'style-loader!css-loader'}
+       {
+         test: /\.css$/, use: ['style-loader','css-loader']
+       },
+       {
+         test: /\.js$/,
+         exclude: /node_modules/,
+         use: 'babel-loader'
+       }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    stats: "errors-only"
   }
 }
