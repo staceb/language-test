9455853a9e6d27d1e30e5acae0258f087d1d69fb
David Cisneros <d.cisneros849@gmail.com>
2018-01-01 00:54:53 -0500

    learning how to extend react components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ const css = require('./app.css')
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const rootElement = document.getElementById('root')
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> My Div </h1>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById('root')
+  < App />,
+  rootElement
 );
